07654d235bfa77f4ef0f528c5f93a47389a1ac6d
simelane-jpd <201575227@student.uj.ac.za>
2022-02-22 15:25:16 +0200

    added new game started

---
+++ index.js
@@ -0,0 +1,67 @@
+let randomNum;
+const guessnum = document.querySelector(".guessnum");
+const guessBtn = document.querySelector(".guessBtn");
+const message = document.querySelector(".message");
+const newGame = document.querySelector(".newGameMsg");
+
+function newGameBtn() {
+    randomNum = Math.ceil(Math.random() * 100);
+    message.innerHTML = '';
+    guessnum.value = undefined;
+    newGame.innerHTML = 'New game started!';
+    setTimeout(function(){
+        newGame.innerHTML = "";
+    }, 4000);
+  }
+  
+  
+  newGameBtn();
+
+  function guessBtnClicked(numberguessed) {
+    //const numberguessed = Number(guessnum.value);
+    const low = 'Your guess is too low';
+    const high = 'Your guess is too high';
+    const correct = `Correct, the secret number is ${randomNum}`;
+    let answer =
+      numberguessed == randomNum
+        ? correct
+        : numberguessed > randomNum
+        ? high
+        : low;
+    message.innerHTML = answer;
+    answer == correct ? setTimeout(newGameBtn, 4000) : undefined;
+  }
+  
+
+
+
+
+
+
+
+
+   
+    
+
+
+    //setTimeout(function(){
+       // location.reload();
+    // newGame.innerHTML = "";
+  //},4000);
+  
+    
+//}
+      
+
+    
+
+
+
+
+
+
+guessBtn.addEventListener('click', () => {
+    guessBtnClicked(guessnum.value);
+  });
+ 
+//guessBtn.addEventListener('click', guessBtnClicked(guess.value))
