2db4d8f813141f1eed0333bfcba9dffe2b1ad452
simelane-jpd <201575227@student.uj.ac.za>
2022-03-09 11:16:03 +0200

    added unit tests

---
+++ functions.js
@@ -0,0 +1,64 @@
+let randomNum;
+const guessnum = document.querySelector(".guessnum");
+const guessBtn = document.querySelector(".guessBtn");
+const message = document.querySelector(".message");
+const newGame = document.querySelector(".newGameMsg");
+
+function newGameBtn() {
+
+
+  setTimeout(function () {
+    message.innerHTML = ""
+  }, 4000)
+
+}
+function outRange() {
+
+}
+function guessBtnClicked(numberguessed) {
+  const numberguessed = Number(randomNum.value);
+
+  if (numberguessed > 100 || numberguessed < 1) {
+    message.innerHTML = 'guess must be between 1 and 100';
+
+    setTimeout(function () {
+      location.reload()
+    }, 4000);
+
+
+  } else if (numberguessed < randomNum) {
+    message.innerHTML = 'Your guess is too low';
+    setTimeout(function () {
+      message.innerHTML = ""
+    }, 4000)
+
+  } else if (numberguessed > randomNum) {
+    message.innerHTML = 'Your guess is too high';
+    setTimeout(function () {
+      message.innerHTML = ""
+    }, 4000)
+
+  
+
+
+
+} else {message.innerHTML = `Correct, the secret number is ${randomNum}`
+
+newGameBtn()
+setTimeout(function () {
+  message.innerHTML = ""
+  location.reload();
+  message.innerHTML = 'New game started!';
+}, 4000)
+}
+}
+  
+
+
+
+
+guessBtn.addEventListener('click', () => {
+  guessBtnClicked(guessnum.value);
+});
+
+
---
+++ test/functions.tests.js
@@ -0,0 +1,40 @@
+describe('The Gussing game function' , function() {
+   
+    describe('', function() {
+    it ('should return "your guess is too low" if the random number is less than the secret number', function() {
+        let playGame = newGameBtn();
+            playGame.guessBtnClicked(0);
+            assert.equal(playGame.guessBtnClicked('Your guess is too low'))
+        });
+
+        it('should return "your guess is too high" if the random number is less than the secret number', function() {
+            let playGame = newGameBtn();
+            playGame.guessBtnClicked(100);
+            assert.equal(playGame.guessBtnClicked('Your guess is too high'))
+        });
+
+        it('should return Correct! if the random number is equal to the secret number' , function() {
+            let playGame = newGameBtn();
+            playGame._randomNum = 23;
+            playGame.guessBtnClicked(23);
+            assert.equal(playGame.guessBtnClicked(23))
+        });
+
+        it('should return "guess must be between 1 and 100" if the random number is less or more than the range of 1 and 100', function() {
+            let playGame = newGameBtn();
+            playGame.guessBtnClicked(-2);
+            assert.equal(playGame.guessBtnClicked('guess must be between 1 and 100'))
+        });
+
+
+        it('should return "guess must be between 1 and 100" if the random number is more than the range of 1 and 100', function() {
+            let playGame = newGameBtn();
+            playGame.guessBtnClicked(101);
+            assert.equal(playGame.guessBtnClicked('guess must be between 1 and 100'))
+        });
+
+        
+
+    });
+
+})
